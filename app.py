e0eea4a118470903d47ecf508940480a82f5889e
CosmoCocktail <jrtanuki00@gmail.com>
2026-06-11 19:44:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # ======================
 # 페이지 설정
@@ -13,6 +14,11 @@ st.set_page_config(
 # ======================
 # 세션 초기화
 # ======================
+QUESTION_FILE = "questions.xlsx"
+
+questions_df = pd.read_excel(
+    QUESTION_FILE
+)
 
 if "page" not in st.session_state:
     st.session_state.page = "home"
@@ -48,11 +54,53 @@ if st.session_state.page == "home":
         st.rerun()
 
 # ======================
-# 질문 페이지(임시)
-# ======================
+elif st.session_state.page == "question":
+
+    current = questions_df.iloc[
+        st.session_state.question_idx
+    ]
+
+    st.markdown(
+        f"""
+        <h2 style='text-align:center'>
+        {current['질문']}
+        </h2>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.write("")
+
+    st.info("질문 이미지 영역")
+
+    st.write("")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+
+        if st.button(
+            current["답변A"],
+            use_container_width=True
+        ):
+
+            st.session_state.question_idx += 1
+
+            st.rerun()
+
+    with col2:
+
+        if st.button(
+            current["답변B"],
+            use_container_width=True
+        ):
+
+            st.session_state.question_idx += 1
+
+            st.rerun()
 
 elif st.session_state.page == "question":
 
-    st.title("질문 페이지")
+    st.success("모든 질문이 종료되었습니다.")
 
-    st.write("다음 단계에서 엑셀 데이터를 연결합니다.")
+    st.write("다음 단계에서 결과 페이지를 연결합니다.")

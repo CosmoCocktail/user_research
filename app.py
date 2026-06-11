e73773c07a3518063033b12e9747df6b21679c7d
CosmoCocktail <jrtanuki00@gmail.com>
2026-06-11 15:46:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -95,15 +95,10 @@ if "scores" not in st.session_state:
 # -------------------
 
 def select_answer(score_data):
-
-```
-for character, score in score_data.items():
+    for character, score in score_data.items():
     st.session_state.scores[character] += score
-
-st.session_state.current_question += 1
-
-st.rerun()
-```
+    st.session_state.current_question += 1
+    st.rerun()
 
 # -------------------
 

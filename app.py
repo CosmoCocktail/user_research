c4eb4ab42dc9ca86264622f548e5c139df3ab65d
CosmoCocktail <jrtanuki00@gmail.com>
2026-06-11 19:51:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,6 @@ if st.session_state.page == "home":
         st.rerun()
 
 # ======================
-st.write(st.session_state)
 elif st.session_state.page == "question":
 
     current = questions_df.iloc[

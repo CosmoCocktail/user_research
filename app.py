58b2d0444fd4fcbd0dd3206fc83f01fd2c724d4b
CosmoCocktail <jrtanuki00@gmail.com>
2026-06-12 17:28:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ elif st.session_state.page == "result":
     st.write("")
 
     # 결과 캐릭터 이미지
-    img_path = f"{character}.png"
+    img_path = f"result_page/{character}.png"
     if os.path.exists(img_path):
         show_image(img_path)
     else:
